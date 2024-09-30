8c3f1d03992d21fefe21b3d4d7a94cf346eee68c
Danillo Santos <danillomdsti@gmail.com>
2024-09-30 01:29:28 -0300

    feature/initial_resolve_problem feature - initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,6 @@
-import entities.Triangle;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        Triangle x, y;
-
-        x = new Triangle();
-        y = new Triangle();
-
-        System.out.println("Digite os valores do triangulo X: ");
-        x.a = sc.nextDouble();
-        x.b = sc.nextDouble();
-        x.c = sc.nextDouble();
-
-        System.out.println("Digite os valores do triangulo Y: ");
-        y.a = sc.nextDouble();
-        y.b = sc.nextDouble();
-        y.c = sc.nextDouble();
-
-        double areaX = x.area();
-
-        double areaY = y.area();
-
-        System.out.printf("Area do triangulo X: %.4f%n", areaX);
-        System.out.printf("Area do triangulo Y: %.4f%n", areaY);
-
-        if (areaX > areaY) {
-            System.out.println("A maior áre do triangulo: X");
-        } else {
-            System.out.println("A maior áre do triangulo: Y");
-        }
-
-
-        sc.close();
 
     }
 }
