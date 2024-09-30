a12510dacac3edc77e4e606ac9e5e8da0b9255ef
Danillo Santos <danillomdsti@gmail.com>
2024-09-30 00:55:04 -0300

    feature/problem_with_oop feature - resolve problem with oop

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,31 @@
+import entities.Triangle;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        double xA, xB, xC, yA, yB, yC;
+        Triangle x, y;
+
+        x = new Triangle();
+        y = new Triangle();
 
         System.out.println("Digite os valores do triangulo X: ");
-        xA = sc.nextDouble();
-        xB = sc.nextDouble();
-        xC = sc.nextDouble();
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
 
         System.out.println("Digite os valores do triangulo Y: ");
-        yA = sc.nextDouble();
-        yB = sc.nextDouble();
-        yC = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
 
-        double aP = (xA + xB + xC) / 2;
-        double areaX = Math.sqrt(aP * (aP - xA) * (aP - xB) * (aP - xC));
+        double aP = (x.a + x.b + x.c) / 2;
+        double areaX = Math.sqrt(aP * (aP - x.a) * (aP - x.b) * (aP - x.c));
 
-        double yP = (yA + yB + yC) / 2;
-        double areaY = Math.sqrt(yP * (yP - yA) * (yP - yB) * (yP - yC));
+        double yP = (y.a + y.b + y.c) / 2;
+        double areaY = Math.sqrt(yP * (yP - y.a) * (yP - y.b) * (yP - y.c));
 
         System.out.printf("Area do triangulo X: %.4f%n", areaX);
         System.out.printf("Area do triangulo Y: %.4f%n", areaY);
