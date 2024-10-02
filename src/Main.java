f3aace307d8fd6df1ebfa449b634fb1061c96c5a
Danillo Santos <danillomdsti@gmail.com>
2024-10-02 00:01:58 -0300

    feature/stock feature - create project stock

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product name: ");
+        System.out.print("Name: ");
+        product.name = sc.nextLine();
+
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+        System.out.println("Update data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        int removeQuantity = sc.nextInt();
+        product.removeProducts(removeQuantity);
+        System.out.println("Update data: " + product);
+
+
+        sc.close();
 
     }
 }
