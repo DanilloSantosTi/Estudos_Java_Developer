804fb20d32beff2bb5ddd7886d6cfe5677869149
Danillo Santos <danillomdsti@gmail.com>
2024-10-14 01:21:38 -0300

    feature/static_members_OOP feature - resolved exercise

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import util.Calculator;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        double circumference = Calculator.circumference(radius);
+
+        double volume = Calculator.volume(radius);
+
+        System.out.printf("Circumference: %.2f%n", circumference);
+        System.out.printf("Volume: %.2f%n", volume);
+
+        sc.close();
     }
 }
